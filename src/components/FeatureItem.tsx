ddc141cac5317b7df3e1e4d5c6c79ad7cd8a0acf
Precious OSSAI <theossaiprecious@gmail.com>
2023-08-23 20:44:03 +0100

    feat: add core functionality and example

---
+++ src/components/FeatureItem.tsx
@@ -0,0 +1,95 @@
+import React from "react";
+
+export type AdFeature = {
+  id: string;
+  title: string;
+  description: string;
+  imageUrl: string;
+};
+
+type Props = {
+  intersectingEntries?: Record<string, boolean>;
+  feature: AdFeature & { ref: React.RefObject<HTMLDivElement> };
+};
+export const FeatureItem: React.FC<Props> = ({
+  intersectingEntries,
+  feature,
+}) => {
+  const isIntersecting = intersectingEntries?.[feature.id];
+
+  return (
+    <div
+      id="feature-item"
+      className="fsr-flex lg:fsr-gap-32 lg:fsr-flex-row fsr-flex-col fsr-group last:lg:fsr-h-auto fsr-relative fsr-isolate"
+      data-feature-id={feature.id}
+      ref={feature.ref}
+    >
+      {/* Mobile Line follower */}
+      <div
+        className={`${
+          isIntersecting ? "fsr-h-full" : "fsr-h-0"
+        } fsr-w-[2px] fsr-translate-x-[-0.5px] fsr-z-10 fsr-absolute fsr-top-0 fsr-bg-blue-600 fsr-left-0 fsr-block lg:fsr-hidden fsr-transition fsr-origin-top fsr-transform fsr-duration-700`}
+      ></div>
+      {/* Bullet background */}
+      <div className="fsr-w-4 fsr-h-7 fsr-z-10 fsr-bg-white fsr-scale-y-150 fsr-absolute fsr-left-0 lg:fsr-left-[50%] -fsr-translate-x-2 fsr-translate-y-[-0.4375rem]"></div>
+      {/* Bullet point */}
+      <div
+        className={`${
+          isIntersecting
+            ? " fsr-ring-blue-500 fsr-ring  fsr-bg-blue-500"
+            : " fsr-bg-gray-300"
+        } fsr-transition fsr-duration-300 fsr-w-4 fsr-h-4 fsr-z-10 -fsr-translate-x-2 fsr-absolute fsr-left-0 lg:fsr-left-[50%] fsr-rounded-full fsr-border-2 fsr-border-white`}
+      ></div>
+
+      {/* Image */}
+      <div className="fsr-flex-1 fsr-w-full fsr-order-2 lg:fsr-order-1">
+        <div
+          className={`${
+            isIntersecting
+              ? "fsr-opacity-100 fsr-scale-100"
+              : "lg:fsr-opacity-0 lg:fsr-scale-50"
+          } fsr-bg-gray-50 fsr-transition fsr-duration-300 fsr-border fsr-border-gray-200 fsr-p-8 fsr-rounded-md fsr-flex fsr-justify-center fsr-items-start lg:-fsr-mb-96 fsr-sticky fsr-top-64 group-last:fsr-mb-0 fsr-z-20 fsr-ml-6 lg:fsr-ml-0 fsr-mb-16`}
+        >
+          <img
+            className="fsr-w-48 fsr-object-contain lg:fsr-w-64"
+            src={feature.imageUrl}
+          />
+        </div>
+      </div>
+      {/* Info */}
+      <div className="fsr-flex-1 fsr-w-full fsr-self-stretch fsr-order-1 lg:fsr-order-2 fsr-text-left">
+        <div className="fsr-h-full">
+          <div className="fsr-flex-1 fsr-h-full fsr-ml-6 lg:fsr-pl-0">
+            {/* Title */}
+            <h4
+              className={`${
+                isIntersecting ? "fsr-text-blue-600" : "fsr-text-gray-300"
+              } fsr-transition fsr-duration-300 fsr-text-2xl lg:fsr-text-3xl -fsr-mt-3`}
+              ref={feature.ref}
+              id={feature.id}
+            >
+              {feature.title}
+            </h4>
+            {/* Description */}
+            <p
+              className={`${
+                isIntersecting ? "fsr-text-gray-600" : "fsr-text-gray-300"
+              } fsr-transition fsr-duration-300 fsr-mt-4`}
+            >
+              {feature.description}
+            </p>
+            {/* Link */}
+            <button
+              className={`${
+                isIntersecting ? "fsr-text-blue-600" : "fsr-text-gray-300"
+              } fsr-text-md fsr-font-medium fsr-mt-4 fsr-mb-8 lg:fsr-mb-32 lg:group-last:fsr-mb-0 fsr-transition fsr-duration-300`}
+            >
+              Learn more
+              <span className="fsr-ml-1">&gt;</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/FeatureScroller.tsx
+++ src/components/FeatureScroller.tsx
@@ -1,5 +1,104 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { FeatureItem, AdFeature } from "./FeatureItem";
 
-export const FeatureScroller: React.FC = () => {
-  return <div>Feature scroller</div>;
+type Props = {
+  features?: AdFeature[];
+};
+
+const defaultdemoFeatures = [
+  {
+    id: "feature1",
+    title: "Example Title",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+          vitae asperiores non deserunt possimus impedit aliquam quae ullam
+          ipsam, voluptate nam in itaque voluptatibus? Praesentium odit a
+          aliquam quod debitis`,
+    imageUrl: "https://placehold.co/600x400",
+  },
+];
+
+export const FeatureScroller: React.FC<Props> = ({
+  features = defaultdemoFeatures,
+}) => {
+  const [intersectingEntries, setIntersectingEntries] = useState<
+    Record<string, boolean>
+  >({});
+  const bulletFaceRef = useRef<HTMLDivElement>(null);
+  const featureList = features.map((feature) => ({
+    ...feature,
+    ref: useRef<HTMLDivElement>(null),
+  }));
+
+  const handleFeatureScrolling = () => {
+    const bulletFaceElementTop =
+      bulletFaceRef?.current?.getBoundingClientRect().top ?? 700;
+
+    document.querySelectorAll("#feature-item").forEach((featureElement) => {
+      const featureElementTop = featureElement.getBoundingClientRect().top;
+      const featureId = featureElement.getAttribute(
+        "data-feature-id"
+      ) as string;
+
+      if (bulletFaceElementTop >= featureElementTop) {
+        setIntersectingEntries({
+          [featureId]: true,
+        });
+      }
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleFeatureScrolling);
+    setIntersectingEntries({ [featureList[0].id]: true });
+import React from "react";
+    return () => {
+      window.removeEventListener("scroll", handleFeatureScrolling);
+    };
+  }, []);
+
+  return (
+    <div className="fsr-relative fsr-isolate lg:-fsr-mt-80">
+      {/* Line divider */}
+      <div
+        className="fsr-w-[2px] fsr-translate-x-[-0.5px] fsr-h-full -fsr-z-10 fsr-absolute fsr-from-gray-200 fsr-left-0 lg:fsr-left-[50%] fsr-bg-repeat-y"
+        style={{
+          backgroundImage: `linear-gradient(
+          180deg,
+          transparent,
+          transparent 50%,
+          var(--fsr-gradient-from) 50%,
+          var(--fsr-gradient-from) 100%
+        )`,
+          backgroundSize: "2px 10px",
+        }}
+      ></div>
+      {/* Top line follower mask */}
+      <div className="fsr-bg-gradient-to-b fsr-from-white fsr-via-white fsr-to-transparent fsr-via-[85%] fsr-top-0 fsr-inset-x-0 fsr-w-full fsr-h-64 fsr-absolute"></div>
+      {/* Line follower */}
+      <div className="fsr-w-[2px] fsr-translate-x-[-0.5px] fsr-h-64 -fsr-z-10 fsr-sticky fsr-top-0 fsr-bg-blue-600 fsr-left-4 lg:fsr-left-[50%] fsr-hidden lg:fsr-block"></div>
+      {/* Bullet face */}
+      <div
+        ref={bulletFaceRef}
+        // className="fsr-transition fsr-duration-300 fsr-w-7 fsr-h-7 fsr-z-20 fsr-translate-x-[-0.875rem] fsr-sticky fsr-top-[37%] lg:fsr-top-64 fsr-left-4 lg:fsr-left-[50%] fsr-rounded-full fsr-border-2 fsr-border-white fsr-ring-blue-500 fsr-ring fsr-bg-blue-500 lg:fsr-block fsr-invisible lg:fsr-visible"
+        className="fsr-transition fsr-duration-300 fsr-w-7 fsr-h-7 fsr-z-20 fsr-translate-x-[-0.875rem] fsr-sticky fsr-top-[37%] lg:fsr-top-64 fsr-left-4 lg:fsr-left-[50%] fsr-rounded-full fsr-border-2 fsr-border-white fsr-ring-blue-500 fsr-ring fsr-bg-blue-500 lg:fsr-block fsr-invisible lg:fsr-visible"
+      >
+        <div
+          id="ball"
+          // className="fsr-w-full fsr-h-full fsr-bg-blue-900 fsr-rounded-full"
+        ></div>
+        {/* <div className="fsr-w-full fsr-h-full fsr-bg-blue-900 fsr-rounded-full"></div> */}
+      </div>
+      {/* Feature listing */}
+      <div className="-fsr-mt-4">
+        {featureList &&
+          featureList.map((feature) => (
+            <FeatureItem
+              key={feature.id}
+              feature={feature}
+              intersectingEntries={intersectingEntries}
+            ></FeatureItem>
+          ))}
+      </div>
+    </div>
+  );
 };
