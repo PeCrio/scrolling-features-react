c2e61eea0675ddc3bb1b5ccc65ac43c434216682
Precious OSSAI <theossaiprecious@gmail.com>
2023-11-01 22:17:45 +0000

    feat: update design

--- src/App.tsx
+++ src/App.tsx
@@ -35,6 +35,25 @@ const CustomTextComponent: React.FC<{
   </div>
 );
 
+const PropExplainer: React.FC<{
+  isIntersecting?: boolean;
+  title: string;
+  description: string;
+}> = ({ title, description, isIntersecting }) => (
+  <div className="lg:sfr-max-w-xs sfr-container sfr-border-dashed sfr-border-[rgba(255,255,255,0.3)] sfr-border sfr-my-2 lg:sfr-my-0 sfr-p-4 sfr-rounded-lg">
+    <h2
+      className={`sfr-text-lg lg:sfr-text-2xl sfr-font-bold sfr-text-white sfr-drop-shadow-md ${
+        isIntersecting ? "sfr-text-indigo-500" : "sfr-text-white"
+      }`}
+    >
+      {title}
+    </h2>
+    <p className="sfr-text-[rgba(255,255,255,0.5)] sfr-text-md sfr-mt-2">
+      {description}
+    </p>
+  </div>
+);
+
 const SimpleCustomIndicator: React.FC<{ isIntersecting: boolean }> = ({
   isIntersecting,
 }) => (
@@ -154,9 +173,9 @@ function App() {
           />
         </div>
       </div>
-      <div className="sfr-pb-48">
-        <div className="sfr-container sfr-flex sfr-justify-center sfr-mx-auto">
-          <div className="sfr-text-center sfr-mb-14 sfr-max-w-md">
+      <div className="lg:sfr-pb-96 lg:sfr-pt-32 sfr-pb-16">
+        <div className="sfr-container sfr-mx-auto">
+          {/* <div className="sfr-text-center sfr-mb-14 sfr-max-w-md sfr-relative sfr-flex sfr-flex-col">
             <h1 className="sfr-text-gray-200 sfr-text-3xl lg:sfr-text-5xl sfr-font-bold">
               Props and options
             </h1>
@@ -164,41 +183,50 @@ function App() {
               This package comes with customizable options and props to extend
               for different use-cases
             </p>
+          </div> */}
+          <div className="sfr-container sfr-flex sfr-justify-center sfr-mx-auto ">
+            <div className="sfr-text-center lg:sfr-mb-14 sfr-max-w-2xl sfr-relative sfr-p-4 lg:sfr-p-16 sfr-flex sfr-flex-col">
+              <div className="sfr-mb-4">
+                <h1 className="sfr-text-gray-200 sfr-text-3xl lg:sfr-text-5xl sfr-font-bold">
+                  Props and options
+                </h1>
+                <p className="sfr-text-[rgba(255,255,255,0.6)] lg:sfr-text-xl sfr-mt-4">
+                  This package comes with customizable options and props to
+                  extend for different use-cases
+                </p>
+              </div>
+              <div className="lg:sfr-top-0 lg:sfr-left-0 lg:sfr-absolute lg:sfr-translate-x-[-70%] lg:sfr-translate-y-[-80%] sfr-text-left">
+                <PropExplainer
+                  title="features"
+                  description="List of items to be displayed "
+                />
+              </div>
+              <div className="lg:sfr-top-0 lg:sfr-right-0 lg:sfr-absolute lg:sfr-translate-x-[70%] lg:sfr-translate-y-[-80%] sfr-text-left">
+                <PropExplainer
+                  title="trackingBall"
+                  description="The ball that follows the top of the screen"
+                />
+              </div>
+              <div className="lg:sfr-bottom-0 lg:sfr-left-0 lg:sfr-absolute lg:sfr-translate-x-[-100%] lg:sfr-translate-y-[80%] sfr-text-left">
+                <PropExplainer
+                  title="trackingLineColor"
+                  description="Highlight color of the active"
+                />
+              </div>
+              <div className="lg:sfr-bottom-0 lg:sfr-self-center lg:sfr-absolute lg:sfr-translate-y-[150%] sfr-text-left">
+                <PropExplainer
+                  title="trackingLineFadeColor"
+                  description="Ideally the background color, helps add a fading effect to the beginning of the tracking line"
+                />
+              </div>
+              <div className="lg:sfr-bottom-0 lg:sfr-right-0 lg:sfr-absolute lg:sfr-translate-x-[100%] lg:sfr-translate-y-[80%] sfr-text-left">
+                <PropExplainer
+                  title="lineTrail"
+                  description="The center line for desktop and left line for mobile"
+                />
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="sfr-container sfr-mx-auto">
-          <ul className="sfr-text-white">
-            <li>
-              <CustomTextComponent
-                title="features"
-                description="List of items to be displayed "
-              />
-            </li>
-            <li>
-              <CustomTextComponent
-                title="trackingBall"
-                description="The ball that follows the top of the screen"
-              />
-            </li>
-            <li>
-              <CustomTextComponent
-                title="trackingLineColor"
-                description="Highlight color of the active"
-              />
-            </li>
-            <li>
-              <CustomTextComponent
-                title="trackingLineFadeColor"
-                description="Ideally the background color, helps add a fading effect to the beginning of the tracking line"
-              />
-            </li>
-            <li>
-              <CustomTextComponent
-                title="lineTrail"
-                description="The center line for desktop and left line for mobile"
-              />
-            </li>
-          </ul>
         </div>
       </div>
       <footer className="sfr-container sfr-mx-auto sfr-py-4 sfr-border-t sfr-border-[rgba(255,255,255,0.3)]">
